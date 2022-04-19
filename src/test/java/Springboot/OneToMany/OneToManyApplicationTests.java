424737c2f61db25c01b8440d31ffa70392f8dc88
manishbidari4 <101552530+manishbidari4@users.noreply.github.com>
2022-04-19 17:26:54 +0545

    One to Many Mapping

---
+++ src/test/java/Springboot/OneToMany/OneToManyApplicationTests.java
@@ -0,0 +1,13 @@
+package Springboot.OneToMany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneToManyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
